7254dbe1d3eb086747b516066aeb9d269aec9536
Dinesh Elumalai <dineshkumar.edk@gmail.com>
2018-03-09 14:23:39 -0800

    Quick Sort

--- Java Algorithms/src/com/dinesh/elumalai/java/alg/sorting/QuickSort.java
+++ Java Algorithms/src/com/dinesh/elumalai/java/alg/sorting/QuickSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 /**
  * 
- * @author Dinesh Elumalai
+ * @author Dinesh Elumalai 
  * Below is an simple illustration of Quick Sort 
  * Best time Complexity : O(nlogn)
  * Avg time Complexity : O(nlogn)
@@ -12,44 +12,61 @@ import java.util.Arrays;
  * Worst Space Complexity : O(logn)
  */
 public class QuickSort {
+	
 	public static void main(String[] args) {
-		int[] x = { 9, 2, 4, 7, 3, 7, 10 };
+		int[] x = { 9, 2, 4, 10, 8, 6 };
 		System.out.println(Arrays.toString(x));
 		int low = 0;
 		int high = x.length - 1;
-		quickSort(x, low, high);
+		sort(x, low, high);
 		System.out.println(Arrays.toString(x));
 	}
 
-	public static void quickSort(int[] arr, int low, int high) {
-		if (arr == null || arr.length == 0)
-			return;
-		if (low >= high)
-			return;
-		// pick the pivot
-		int middle = low + (high - low) / 2;
-		int pivot = arr[middle];
-		// make left < pivot and right > pivot
-		int i = low, j = high;
-		while (i <= j) {
-			while (arr[i] < pivot) {
+	 /*
+	 * This function takes last element as pivot, places the pivot element at
+	 * its correct position in sorted array, and places all smaller (smaller
+	 * than pivot) to left of pivot and all greater elements to right of pivot
+	 */
+	static int partition(int arr[], int low, int high) {
+		int pivot = arr[high];
+		int i = (low - 1); // index of smaller element
+		for (int j = low; j < high; j++) {
+			// If current element is smaller than or
+			// equal to pivot
+			if (arr[j] <= pivot) {
 				i++;
-			}
-			while (arr[j] > pivot) {
-				j--;
-			}
-			if (i <= j) {
+
+				// swap arr[i] and arr[j]
 				int temp = arr[i];
 				arr[i] = arr[j];
 				arr[j] = temp;
-				i++;
-				j--;
 			}
 		}
-		// recursively sort two sub parts
-		if (low < j)
-			quickSort(arr, low, j);
-		if (high > i)
-			quickSort(arr, i, high);
+
+		// swap arr[i+1] and arr[high] (or pivot)
+		int temp = arr[i + 1];
+		arr[i + 1] = arr[high];
+		arr[high] = temp;
+
+		return i + 1;
 	}
+
+	/*
+	 * The main function that implements QuickSort() arr[] --> Array to be
+	 * sorted, low --> Starting index, high --> Ending index
+	 */
+	static void sort(int arr[], int low, int high) {
+		if (low < high) {
+			/*
+			 * pi is partitioning index, arr[pi] is now at right place
+			 */
+			int pi = partition(arr, low, high);
+
+			// Recursively sort elements before
+			// partition and after partition
+			sort(arr, low, pi - 1);
+			sort(arr, pi + 1, high);
+		}
+	}
+
 }
